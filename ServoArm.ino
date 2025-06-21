77a3485fcee36d1f5e926b3b1091295461f5bd36
Gansemarsch <>
2025-06-21 13:15:32 -0400

    Working Ultrasonic

--- ServoArm.ino
+++ ServoArm.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <Servo.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <Ultrasonic.h>
 
 
 
@@ -22,7 +23,13 @@
 #define SERVO3_PIN 5
 #define SERVO4_PIN 6
 
+//********** Ultrasonic **************
+//
+Ultrasonic ultrasonic(12, 13);
+long lastSonicRange = 0;
 
+//********** Servo **************
+//
 Servo Servo1;
 Servo Servo2;
 Servo Servo3;
@@ -64,50 +71,65 @@ class AsyncServo
   }
 };
 
+AsyncServo servo1(&Servo1, SERVO1_PIN, 3, 3, 10, 0);
+AsyncServo servo2(&Servo2, SERVO2_PIN, 3, 3, 10, 0);
+AsyncServo servo3(&Servo3, SERVO3_PIN, 3, 3, 10, 0);
+AsyncServo servo4(&Servo4, SERVO4_PIN, 3, 3, 10, 0);
 
-int Servo1_pos = 0;  // variable to store the servo position
-int Servo2_pos = 0;  // variable to store the servo position
-int Servo3_pos = 0;  // variable to store the servo position
-int Servo4_pos = 0;  // variable to store the servo position
-
-int newJointPos = 10;
 
 // Initialize at the default i2c address, we are only using one expander
 //Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   //Setup Servo Driver
   Servo1.attach(SERVO1_PIN);  
   Servo2.attach(SERVO2_PIN); 
   Servo3.attach(SERVO3_PIN);
   Servo4.attach(SERVO4_PIN);
     //Setup Arm Parameters
-
+  ultrasonic.setTimeout(40000UL);
   //Setup Ultrasonic Sensor
-  pinMode(LED_BUILTIN, OUTPUT);
+  //pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SERVO1_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+
   //Arm.setJointPos(0, newJointPos);
   //Arm.setJointPos(1, newJointPos);
   //newJointPos += 10;
 
   //come(Servo1);
   //come(Servo2);
-
+  sonicDirection(ultrasonic.read());
   //if (newJointPos >= 175) {
   //  newJointPos = 15;
   //}
+  delay(1000);
 }
 
-
+// Returns 0,1,2 if the range is decreasing, stable, increasing
+int sonicHysterisis = 20;
+int sonicDirection(long reading){
+  Serial.println(reading);
+  if(reading <= (lastSonicRange - sonicHysterisis)){
+    Serial.println("Range Decreasing");
+    lastSonicRange = reading;
+    return 0;
+  }else if(reading >= lastSonicRange + sonicHysterisis){
+    Serial.println("Range increasing");
+    lastSonicRange = reading;
+    return 2;
+  }else {
+    Serial.println("Range stable");
+    lastSonicRange = reading;
+    return 1;
+  }
+  lastSonicRange = reading;
+}
 
 
 void come(Servo movingServo, int pos) {
